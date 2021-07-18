bd1d0dfb7fef40e08a431c5a35018ac7827823f4
nano <76194982+nano2nano@users.noreply.github.com>
2021-07-18 22:52:29 +0900

    Invalidate firebase analytics.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,17 @@
-import 'package:firebase_analytics/firebase_analytics.dart';
-import 'package:firebase_analytics/observer.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'search_form.dart';
 
-Future<void> main() async {
-  await Firebase.initializeApp();
+void main() {
+  // await Firebase.initializeApp();
   runApp(ProviderScope(
     child: MyApp(),
   ));
 }
 
 class MyApp extends StatelessWidget {
-  static FirebaseAnalytics analytics = FirebaseAnalytics();
+  // static FirebaseAnalytics analytics = FirebaseAnalytics();
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -26,9 +23,9 @@ class MyApp extends StatelessWidget {
       home: const Scaffold(
         body: SearchForm(),
       ),
-      navigatorObservers: [
-        FirebaseAnalyticsObserver(analytics: analytics),
-      ],
+      // navigatorObservers: [
+      //   FirebaseAnalyticsObserver(analytics: analytics),
+      // ],
     );
   }
 }
