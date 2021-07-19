33aab96a6d84cc991e683618e2ae4a5015d40edf
nano <76194982+nano2nano@users.noreply.github.com>
2021-07-19 13:40:05 +0900

    Init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'search_form.dart';
 
-void main() {
-  // await Firebase.initializeApp();
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(ProviderScope(
     child: MyApp(),
   ));
